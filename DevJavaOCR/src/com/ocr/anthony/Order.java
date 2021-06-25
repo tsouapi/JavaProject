74fbe6980400305f62e7f9bd04df77975b35c110
tsouapi <r1tsouapi@gmail.com>
2021-06-25 18:51:58 +0000

    version with condition first commit

--- DevJavaOCR/src/com/ocr/anthony/Order.java
+++ DevJavaOCR/src/com/ocr/anthony/Order.java
@@ -5,18 +5,25 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
-        System.out.println("Choix de menu");
-        System.out.println("1- poulet");
-        System.out.println("2- boeuf");
-        System.out.println("3- vegetarian");
-        System.out.println("Que souhaitez-vous comme menu?");
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
     }
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-        System.out.println("Vous avez Choisi le menu " + nbMenu);
-
+        if (nbMenu == 1) {
+            System.out.println("Vous avez choisi comme menu : poulet");
+        } else if (nbMenu == 2) {
+            System.out.println("Vous avez choisi comme menu : boeuf");
+        } else if (nbMenu == 3) {
+            System.out.println("Vous avez choisi comme menu : végétarien");
+        } else {
+            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+        }
     }
 }
--- DevJavaOCR/test/com/ocr/anthony/OrderTest.java
+++ DevJavaOCR/test/com/ocr/anthony/OrderTest.java
@@ -1,14 +1,12 @@
 package com.ocr.anthony;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class OrderTest {
 
@@ -26,14 +24,32 @@ public class OrderTest {
 
     Order order = new Order();
 
+
+   @Test
+   public void Given_Chicken_When_DisplayMenuSelected_Then_DisplayChickenSentence() {
+       order.displaySelectedMenu(1);
+       assertEquals("Vous avez choisi comme menu : poulet\n", outContent.toString().replace("\r\n", "\n"));
+   }
     @Test
-    public void Given_Nothing_When_DisplayMenuSelection_Then_ShouldDisplayText() {
-        order.displayAvailableMenu();
-        assertEquals(false, outContent.toString().isEmpty());
+    public void Given_Beef_When_DisplayMenuSelected_Then_DisplayBeefSentence() {
+        order.displaySelectedMenu(2);
+        assertEquals("Vous avez choisi comme menu : boeuf\n", outContent.toString().replace("\r\n", "\n"));
     }
     @Test
-    public void Given_Vegetarian_When_AskForMenuSelected_Then_ShouldDisplayCorrectSentence() {
+    public void Given_Vegetarian_When_DisplayMenuSelected_Then_DisplayVegetarianSentence() {
         order.displaySelectedMenu(3);
-        assertEquals("Vous avez choisi le menu 3\n", outContent.toString());
+        assertEquals("Vous avez choisi comme menu : végétarien\n", outContent.toString().replace("\r\n", "\n"));
     }
+    @Test
+    public void Given_TooBigValue_When_DisplayMenuSelected_Then_DisplayErrorSentence() {
+        order.displaySelectedMenu(15);
+        assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", outContent.toString().replace("\r\n", "\n"));
+    }
+    @Test
+    public void Given_NegativeValue_When_DisplayMenuSelected_Then_DisplayErrorSentence() {
+        order.displaySelectedMenu(-6);
+        assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", outContent.toString().replace("\r\n", "\n"));
+    }
+
+
 }
